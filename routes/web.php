3083d3247d0b71f447fe097c38d7584321f0b82e
AR-Shahin <mdshahinmije96@gmail.com>
2023-02-14 22:36:21 +0600

    d

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get("/ars",[HomeController::class,'index']);
-Route::any('/',[HomeController::class,'index'])->name('home');
+Route::get('/',[HomeController::class,'index'])->name('home');
 Route::get('category/{cat}/subcategory/{subcat}',[HomeController::class,"categorySubcategory"])->name('cat_subcat');
+Route::redirect('/','ars');
