993e8cfaec68ba16d9d2349d9d144f7027a54080
AR-Shahin <mdshahinmije96@gmail.com>
2023-02-15 21:06:15 +0600

    search bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -130,10 +130,10 @@ footer p{
               </li>
 
             </ul>
-            {{-- <form class="d-flex" role="search">
+            <form class="d-flex" role="search">
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
               <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> --}}
+            </form>
           </div>
         </div>
       </nav>
