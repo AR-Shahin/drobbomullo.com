2d45d20bdba5a20ef647bd8061746821031d3a1a
AR-Shahin <mdshahinmije96@gmail.com>
2023-02-15 13:58:47 +0600

    route change

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 
-Route::get("/ars",[HomeController::class,'index']);
-Route::get('/',[HomeController::class,'index'])->name('home');
 Route::get('category/{cat}/subcategory/{subcat}',[HomeController::class,"categorySubcategory"])->name('cat_subcat');
+
+Route::get("/ars",[HomeController::class,'index']);
+
+Route::get('/',[HomeController::class,'index'])->name('home');
+
