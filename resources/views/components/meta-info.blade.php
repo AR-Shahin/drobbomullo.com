2c6a922b1334ddd8dad756d6ba9fe711d2e754ed
AR-Shahin <mdshahinmije96@gmail.com>
2023-02-23 19:57:11 +0600

    meta data update

--- resources/views/components/meta-info.blade.php
+++ resources/views/components/meta-info.blade.php
@@ -2,13 +2,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:image" content="https://drobbomullo.com/images/side-logo.png" />
-    <meta name="description" content="drobbomullo" />
+    <meta name="description" content="দ্রব্যমূল্য.com" />
 
-    <meta property="og:title" content="  drobbomullo | drobbomullo  " />
+    <meta property="og:title" content="  দ্রব্যমূল্য.com | Drobbomullo  " />
 
     <meta name="og:description" content="drobbomullodrobbomullodrobbomullodrobbomullodrobbomullo ">
 
-    <meta property="og:site_name" content="drobbomullo" />
+    <meta property="og:site_name" content="দ্রব্যমূল্য.com" />
 
     <meta property="og:url" content="https://www.facebook.com/sharer/sharer.php?u=http://codeshikhi.org/" />
 
