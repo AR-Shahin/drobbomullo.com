85b6d7216b2b9b7f6c8aae527772dd162cddbd3e
AR-Shahin <mdshahinmije96@gmail.com>
2023-02-27 14:05:24 +0600

    ORM primary key change

--- app/Models/MainTable.php
+++ app/Models/MainTable.php
@@ -12,18 +12,18 @@ class MainTable extends Model
         "link","price"
     ];
     protected $table = "main_table";
-    protected $primaryKey = "item_name";
-    protected $keyType = "string";
+    protected $primaryKey = "item_id";
+    // protected $keyType = "string";
 
     public static function fetchCategories()
     {
         return self::select('category')->groupBy('category')->get();
     }
     public function link(){
-        return $this->belongsTo(Link::class,"item_name","item_name");
+        return $this->belongsTo(Link::class,"item_id","item_id");
     }
 
     public function price(){
-        return $this->belongsTo(Price::class,"item_name","item_name");
+        return $this->belongsTo(Price::class,"item_id","item_id");
     }
 }
